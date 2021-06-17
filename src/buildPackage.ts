42e5c7f195d20940e02af8fa65af49942c15b04f
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2021-06-17 22:01:00 +0530

    fix: dev: master: replaced copySync

--- src/buildPackage.ts
+++ src/buildPackage.ts
@@ -5,7 +5,8 @@ import * as fse from 'fs-extra';
 export async function symlinkNodeModules(pathToPackage: string) {
   try {
     const baseNodeModules = path.join(process.cwd(), 'node_modules');
-    await fse.ensureSymlink(baseNodeModules, pathToPackage);
+    const destNodeModules = path.join(pathToPackage, 'node_modules');
+    await fse.copyFileSync(baseNodeModules, destNodeModules);
     console.log('Symlink Complete.');
   } catch(err) {
     console.error(err);
