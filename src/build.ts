22ad376c3161dae2c084490e2dd320fec048de23
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2021-06-27 19:09:03 +0530

    fix: dev: develop: removed cluster

--- src/build.ts
+++ src/build.ts
@@ -1,71 +1,24 @@
-import cluster from 'cluster';
-import * as os from 'os';
 
-import chalk from 'chalk';
+// import chalk from 'chalk';
 import * as _ from 'lodash';
 import ora from 'ora';
 
 import { getElectronForgePackages } from './lerna';
 
-const CPUS = os.cpus().length;
 
 let spinner: any;
 
 async function parallelAppBuilds() {
   const electronForgePackages = await getElectronForgePackages();
 
-  const apps = _.map(electronForgePackages, (efp) => {
-    return {
-      name: efp.name,
-      location: efp.location
-    };
+  _.map(electronForgePackages, (efp) => {
+    // @ts-ignore
+    buildApp(efp.name, efp.location);
   });
-
-  let appData: any;
-
-  if (cluster.isMaster) {
-    console.log(
-      chalk.yellow(`Number of CPUs is ${CPUS}.`)
-    );
-
-    console.log(
-      chalk.cyan(`Master ${process.pid} is running.`)
-    );
-
-    for (let i = 0; i < CPUS; i++) {
-      if (apps.length > 0) {
-        appData = apps.pop();
-        const worker = cluster.fork();
-        worker.send(appData);
-      }
-    }
-
-    cluster.on('online', (worker) => {
-      console.log(`Worker ${worker.process.pid} is online`);
-    });
-
-    cluster.on('exit', (worker, _code, _signal) => {
-      console.log(`Worker ${worker.process.pid} died`);
-      // if (apps.length > 0) {
-      //   appData = apps.pop();
-      //   cluster.fork();
-      // }
-    });
-  } else {
-    process.on('message', (appData) => {
-      if (appData) {
-        // @ts-ignore
-        buildApp(appData.name, appData.location, () => {
-          process.exit(0);
-        });
-      }
-    });
-  }
 }
 
-async function buildApp(appName: string, appPath: string, callback: Function) {
+async function buildApp(appName: string, appPath: string) {
   console.log(appName, appPath);
-  callback();
 }
 
 export function buildCommand() {
