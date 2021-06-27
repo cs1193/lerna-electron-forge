8223c6bcee46e640867e5ad263079bd6ded5547d
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2021-06-27 11:54:00 +0530

    fix: dev: develop: added init changed to fn

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ export async function run() {
   const yargs = require('yargs/yargs')(process.argv.slice(2));
   yargs
     .usage(`${chalk.bold('lerna-electron-forge')} subcommand [options]`)
-    .command(initCommand)
+    .command(initCommand())
     .demandCommand()
     .help('help')
     .argv;
